8531cd9af2d3ed40d79304a0750a382e00d31cee
Perry Huang <perryhuang2015@gmail.com>
2025-08-21 16:05:43 -0400

    feat: make eslint not complain about cjs

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,12 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig, globalIgnores } from "eslint/config";
+const globals = require("globals");
 
-export default defineConfig([
-    globalIgnores(["dist"]),
+module.exports = [
     {
-        files: ["**/*.{js,jsx}"],
-        extends: [js.configs.recommended],
         languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-            parserOptions: {
-                ecmaVersion: "latest",
-                ecmaFeatures: { jsx: true },
-                sourceType: "module",
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,
             },
         },
-        rules: {
-            "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-        },
     },
-]);
+];
