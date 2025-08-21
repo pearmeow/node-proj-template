42cd38f9146a1b347feb9595c7c4ae6e5eb87a21
Perry Huang <perryhuang2015@gmail.com>
2025-08-21 15:20:33 -0400

    feat: add views, public, and ejs

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("node:path");
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-const path = require("node:path");
 
-app.get("/", (req, res) => res.send("Hello world!"));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.get("/", (req, res) => res.render("index"));
 
 const PORT = 3000;
 
